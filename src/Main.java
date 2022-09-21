e8fbd7f381cb387546feb22afdefbdfc282374a8
abdessalm staili <abdoycode@gmail.com>
2022-09-21 19:40:06 +0100

    in the run of programme added the data of game

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
+import Consoles.Consoles;
+import Ecrans.Ecran;
+import GameCategorie.Game;
+import Posts.Posts;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
+        ArrayList<Ecran> allEcran = new ArrayList<>();
+        allEcran.add(new Ecran("Dell",3));
+        allEcran.add(new Ecran("Hp",1));
+        allEcran.add(new Ecran("Asus",3));
+        allEcran.add(new Ecran("Asus",2));
+
+        ArrayList<Consoles> allConsoles = new ArrayList<>();
+        allConsoles.add(new Consoles("Xbox",4));
+        allConsoles.add(new Consoles("PlayStation5",2));
+        allConsoles.add(new Consoles("Nintendo switch",2));
+
+        ArrayList<Game> allGame = new ArrayList<>();
+        allGame.add(new Game("FIFA","football"));
+        allGame.add(new Game("PES","football"));
+        allGame.add(new Game("Counter-Strike","Guerre"));
+        allGame.add(new Game("Assassin's Creed","Guerre"));
+
+        System.out.println(allGame.get(0).name);
     }
+
+
+
 }
