108b193c4b4e41cd06aff49e77c061eb0b4b3463
abdessalm staili <abdoycode@gmail.com>
2022-09-22 14:59:50 +0100

    add a stocker the post in class stocker

--- src/Posts/Posts.java
+++ src/Posts/Posts.java
@@ -1,35 +1,23 @@
 package Posts;
-import Consoles.Consoles;
-import Ecrans.Ecran;
-import GameCategorie.Game;
-public class Posts {
-    String Ecran;
-    String Game;
-    int horaire;
-    String Console;
-    public Posts(String ecran, String game, String console,int horaire) {
-        this.Console = console;
-        this.Ecran = ecran;
-        this.Game = game;
-        this.horaire= horaire;
+import Horaires.Horaires;
+import Info.Info;
+import java.util.ArrayList;
 
-        ReserverEcran(Ecran);
-        ReserverConsole(Console);
-        ReserverGame(Game);
+public class Posts  {
+    public static ArrayList<Info>  allPost = new ArrayList<>();
+    public Posts(String ecran, String console) {
+        allPost.add(new Info(ecran,console));
+    }
+    public Posts(){}
+
+    public void ReserverHourire(int horaire){
+        Horaires ReserverHoraire=new Horaires();
+        ReserverHoraire.check(String.valueOf(horaire));
     }
 
-    public void ReserverEcran(String nameEcran){
-        Ecran ReserverEcran = new Ecran();
-        System.out.println(ReserverEcran.check(nameEcran));
-    }
 
-    public void ReserverConsole(String NameConsole){
-        Consoles ReserverConsole = new Consoles();
-        System.out.println(ReserverConsole.check(NameConsole));
-    }
 
-    public void ReserverGame(String CategorieName){
-        Game GameName = new Game(CategorieName);
 
-    }
+
+
 }
