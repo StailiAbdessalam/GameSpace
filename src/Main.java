aaedd33186547e41dfda1f717ea15b5f90e98e28
abdessalm staili <abdoycode@gmail.com>
2022-09-22 01:17:40 +0100

    main is a class global to stocket all the chause and run the code

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,12 @@ import Consoles.Consoles;
 import Ecrans.Ecran;
 import GameCategorie.Game;
 import Posts.Posts;
-
 import java.util.ArrayList;
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args)
     {
         ArrayList<Ecran> allEcran = new ArrayList<>();
-        allEcran.add(new Ecran("Dell",3));
+        allEcran.add(new Ecran("Dell",2));
         allEcran.add(new Ecran("Hp",1));
         allEcran.add(new Ecran("Asus",3));
         allEcran.add(new Ecran("Asus",2));
@@ -19,15 +16,17 @@ public class Main {
         allConsoles.add(new Consoles("Xbox",4));
         allConsoles.add(new Consoles("PlayStation5",2));
         allConsoles.add(new Consoles("Nintendo switch",2));
+        Consoles console = new Consoles();
+
 
         ArrayList<Game> allGame = new ArrayList<>();
         allGame.add(new Game("FIFA","football"));
         allGame.add(new Game("PES","football"));
         allGame.add(new Game("Counter-Strike","Guerre"));
         allGame.add(new Game("Assassin's Creed","Guerre"));
-
-        System.out.println(allGame.get(0).name);
+        Posts ReserverPosts = new Posts("Dell","pes","Nintendo switch",2);
     }
+
 
 
 
