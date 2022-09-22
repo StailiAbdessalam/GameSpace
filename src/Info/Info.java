d8d3caabcee0c8494a03e7bf8a4267a6d61f98ac
abdessalm staili <abdoycode@gmail.com>
2022-09-22 01:15:31 +0100

    class to create all the propriete (quantity and name)

---
+++ src/Info/Info.java
@@ -0,0 +1,14 @@
+package Info;
+
+public class Info {
+    public int quantity;
+    public String name;
+
+
+    // constructor to know data of all the stokes
+    public Info(String name, int quantity) {
+        this.name=name;
+        this.quantity=quantity;
+    }
+
+}
