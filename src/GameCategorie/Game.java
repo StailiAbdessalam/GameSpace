3b816ea2a5080eefbe0a24f6f2e71faaedb7e402
abdessalm staili <abdoycode@gmail.com>
2022-09-22 15:09:49 +0100

    getGames to get all the game by categorie

--- src/GameCategorie/Game.java
+++ src/GameCategorie/Game.java
@@ -2,16 +2,21 @@ package GameCategorie;
 import java.util.ArrayList;
 
 public class Game {
-    String name;
     String categorie;
-
-    private  static ArrayList <Game> allGame = new ArrayList<>();
+    ArrayList<String> name = new ArrayList<>();
+    public static ArrayList<Game> allGame = new ArrayList<Game>();
     public Game() {}
-    public Game(String name, String categorie) {
+    public Game(ArrayList<String> name,String categorie) {
         this.name = name;
         this.categorie = categorie;
     }
-    public Game(String name){
-        this.name=name;
+    //method to initial data de Game
+    public void initialDataGame(ArrayList<Game> allame){
+        allGame=allame;
     }
+    //method getGame to get all game by categorie
+    public ArrayList<String> getGames(int categorie){
+        return allGame.get(categorie).name;
+    }
+
 }
