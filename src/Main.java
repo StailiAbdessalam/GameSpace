22a053de9f155b8148fe1f65c7c25b4ebde958a8
abdessalm staili <abdoycode@gmail.com>
2022-09-24 13:53:33 +0100

    main the all programe

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
+import affichage.Affichage;
+
 public class Main {
     public static void main(String[] args)
     {
+        Affichage affichage = new Affichage();
+        affichage.Presantation();
 
     }
 
