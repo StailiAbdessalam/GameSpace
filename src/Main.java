bec699cace5f62d6b1a08a8095a72c0bd2851796
abdessalm staili <abdoycode@gmail.com>
2022-09-24 14:53:45 +0100

    add date the all Game by id the post

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
 import Enums.Consoles;
 import Enums.Ecrans;
 import Enums.Games;
+import GameCategorie.Game;
 import Posts.Posts;
 import affichage.Affichage;
+
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args)
@@ -19,7 +22,14 @@ public class Main {
         post.addPost(String.valueOf(Ecrans.ASUS),String.valueOf(Consoles.PES5),7);
         post.addPost(String.valueOf(Ecrans.SAMSUNG),String.valueOf(Consoles.PES5),8);
         post.addPost(String.valueOf(Ecrans.SAMSUNG),String.valueOf(Consoles.PES5),9);
-        post.getAllPost();
+
+
+        Game game = new Game();
+        game.addGame(String.valueOf(Games.PES),"football", new int[]{1, 2, 3,4,7,8,9});
+        game.addGame(String.valueOf(Games.FIFA),"football", new int[]{1, 2, 3,4,7,8,9});
+        game.addGame(String.valueOf(Games.AssassinCreed),"Guerre", new int[]{5,6});
+        game.addGame(String.valueOf(Games.FIFA),"Guerre", new int[]{5,6});
+
     }
 
 
