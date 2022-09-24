e440e2358281908a926b6a58252c9a45e99141f9
abdessalm staili <abdoycode@gmail.com>
2022-09-24 13:00:46 +0100

    initial working

--- src/Enums/Games.java
+++
@@ -1,8 +0,0 @@
-package Enums;
-
-public enum  Games {
-    FiFa ,
-    PES,
-    CounterStrike,
-    AssassinCreed
-}
--- src/Main.java
+++ src/Main.java
@@ -1,49 +1,10 @@
-import GameCategorie.Game;
-import Horaires.Horaires;
-import Posts.Posts;
-import affichage.Affichage;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Objects;
-
 public class Main {
     public static void main(String[] args)
     {
-        // stock the data of Game available
-        ArrayList<Game> allGame = new ArrayList<>();
-        allGame.add(new Game(new ArrayList<>(Arrays.asList("PES","FIFA")),"Footbal" ));
-        allGame.add(new Game(new ArrayList<>(Arrays.asList("Counter-Strike","Assassin's Creed")),"Guerre"));
-        Game games = new Game();
-        games.initialDataGame(allGame);
-        // stock the post composent a Ecran and Console
-        ArrayList<Posts> allpost = new ArrayList<>();
-        allpost.add(new Posts("Dell","Xbox"));
-        allpost.add(new Posts("Dell","Xbox"));
-        allpost.add(new Posts("Dell","Xbox"));
-        allpost.add(new Posts("HP","Xbox"));
-        allpost.add(new Posts("Samsung","PlayStation5"));
-        allpost.add(new Posts("Samsung","PlayStation5"));
-        allpost.add(new Posts("Asus","PlayStation5"));
-        allpost.add(new Posts("Asus","Nintendo switch"));
-        allpost.add(new Posts("Asus","Nintendo switch"));
-        // stock the creneau available in a arraylist
-        ArrayList<Integer> allCreneau = new ArrayList<>();
-        allCreneau.add(30);
-        allCreneau.add(60);
-        allCreneau.add(120);
-        allCreneau.add(300);
-        allCreneau.add(540);
-        // send a arraylist a another array in Class Horaires to use
-        Horaires initialdata = new Horaires(allCreneau);
-
-        // lance programme
-        Affichage Presantation = new Affichage();
-        Presantation.Menu(allCreneau);
-    }
-
-    public void test(){
 
     }
+
+
 
 
 
