008a57e98d76989495c43d7cb81bb5e40feb7206
abdessalm staili <abdoycode@gmail.com>
2022-09-25 22:19:32 +0100

    create a method check the time of work

--- src/affichage/Affichage.java
+++ src/affichage/Affichage.java
@@ -1,17 +1,23 @@
 package affichage;
-
 import Enums.Games;
 import GameCategorie.Game;
+import GameCategorie.GameDetails;
+import Posts.DetailsPosts;
 import Posts.Posts;
-
-import java.util.Arrays;
-import java.util.HashMap;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Affichage {
     public void Presantation(Game game, Posts post){
         System.out.println("************************  HI in GameSpace  ************************");
         while(true){
+            boolean isWork = this.TimeWork();
+            if (!isWork){
+                System.out.println("we are not available in this time see you to our time");
+                System.exit(0);
+            }
             System.out.println("choose your option :\n1: create a Player \n2: statistical \n3: exit");
             Scanner choix = new Scanner(System.in);
             int choixAdmin = choix.nextInt();
@@ -22,11 +28,15 @@ public class Affichage {
                         continue;
                     }
                     String Game = String.valueOf(Games.values()[choixGame-1]);
-                    choosePost(game.getGameByName(Game));
+                    ArrayList<DetailsPosts> postDisponible = choosePost(game.getGameByName(Game),post);
+                    this.choosePost(postDisponible);
+                    this.InfoPlayer();
+
 
                     return;
                 case 2:
-                       return;
+                    System.out.println("attender version");
+                       continue;
                 case 3:
                     System.out.println("bye See you at our time");
                     System.exit(0);
@@ -60,11 +70,45 @@ public class Affichage {
         }
     }
 
-    public void choosePost(HashMap game){
-        System.out.println("choose your Post");
-        Object ipPost = game.get("idPost");
-        System.out.println(ipPost);
+    public ArrayList<DetailsPosts> choosePost(GameDetails game , Posts posts){
+        ArrayList<DetailsPosts> postdisponible = new ArrayList<>();
+        int[] ipPost = game.getIdPost();
+        for (int ele:ipPost){
+            postdisponible.add(posts.getPostById(ele));
+        }
+        return postdisponible;
 
+    }
 
+    public int choosePost(ArrayList<DetailsPosts> postDisponible){
+        System.out.println("this is Post disponible in your Game :\nchoose your post :");
+
+        for(DetailsPosts i:postDisponible){
+            System.out.println("Post"+i.getId()+": "+"Ecran=>"+i.getEcran()+" and Console=>"+i.getConsole());
+        }
+        Scanner choix = new Scanner(System.in);
+        int choixPost = choix.nextInt();
+        int IdPost = choixPost-1;
+        return IdPost;
+    }
+    public void InfoPlayer(){
+        System.out.println("enter your name :");
+        Scanner input = new Scanner(System.in);
+        String playerName = input.nextLine();
+        System.out.println(playerName);
+    }
+
+    public boolean TimeWork(){
+        DateTimeFormatter forma = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+        LocalDateTime timeNow = LocalDateTime.now();
+        String timeformate = timeNow.format(forma);
+        String[] DetailsTime = timeformate.split(" ");
+        String[]  justTime = DetailsTime[1].split(":");
+        int Hour = Integer.parseInt(justTime[0]);
+        System.out.println(Hour);
+        if(Hour<9 ||Hour >=20 || Hour>=12 && Hour<14 ){
+            return false;
+        }
+        return true;
     }
 }
