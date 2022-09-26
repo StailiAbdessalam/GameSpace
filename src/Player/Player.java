fad2bd1ce15d9a976c9e072dee687c575e095ead
abdessalm staili <abdoycode@gmail.com>
2022-09-26 21:43:53 +0100

    use a new storage the allPlayer to check in the idpost in hashmap

--- src/Player/Player.java
+++ src/Player/Player.java
@@ -1,16 +1,22 @@
 package Player;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public  class Player{
 
     ArrayList<DetailsPlayer> allplayer = new ArrayList<>();
+    HashMap<Integer,DetailsPlayer> AllplayerinHashMap = new HashMap<>();
 
     public void addplayer(String namePlayer, String game, int idpost, String timefinish){
         allplayer.add(new DetailsPlayer(namePlayer,game,timefinish,idpost));
+        AllplayerinHashMap.put(idpost,new DetailsPlayer(namePlayer,game,timefinish,idpost));
     }
 
     public ArrayList<DetailsPlayer> getAllplayer() {
             return allplayer;
+    }
+    public HashMap<Integer,DetailsPlayer> getPlayerByHashMap(int idpost){
+     return AllplayerinHashMap;
     }
 }
