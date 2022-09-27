6ed5a2345b0e9d20e8e26a05e7ad4aa2704a3d17
abdessalm staili <abdoycode@gmail.com>
2022-09-27 20:40:43 +0100

    check the timefinal the Queues list

--- src/affichage/Affichage.java
+++ src/affichage/Affichage.java
@@ -12,11 +12,14 @@ import java.time.format.DateTimeFormatter;
 import java.util.*;
 import Player.DetailsPlayer;
 import Horaires.DetailsHoraires;
+import listUser.ListUser;
+import Queue.Queues;
+
 import java.lang.Thread;
 
 public class Affichage {
     public LocalTime timefinishChange;
-    public void Presantation(Game game, Posts post,Player player,Horaires Time){
+    public void Presantation(Game game, Posts post, Player player, Horaires Time,Queues queueClass){
         System.out.println("************************  HI in GameSpace  ************************");
         while(true){
             boolean isWork = this.TimeWork();
@@ -38,9 +41,9 @@ public class Affichage {
                     String Game = String.valueOf(Games.values()[choixGame-1]);
                     ArrayList<DetailsPosts> postDisponible = this.choosePostDispoible(game.getGameByName(Game),post);
                     // choix 2 of Player
-                    int IdPostChoix = this.choosePost(postDisponible,player);
+                    int IdPostChoix = this.choosePost(postDisponible,player,queueClass);
                     // choix 3 of Player
-                    int choixCreneau = this.chooseCreneau(Time,IdPostChoix,player);
+                    int choixCreneau = this.chooseCreneau(Time,IdPostChoix,player,queueClass);
                     if(choixCreneau==0){
                         continue;
                     }
@@ -48,17 +51,32 @@ public class Affichage {
                     // choix 4 of Horaire
                     String NamePlayer = this.InfoPlayer();
                     String Timefinish = Time.Timefinish(timePlayParMinute,this.timefinishChange);
-                    player.addplayer(NamePlayer,Game,IdPostChoix,Timefinish);
-
+                    //check if post playing
+                    boolean IfpostOccuper = this.checkplayer(IdPostChoix,player);
+                    if(IfpostOccuper){
+                        queueClass.addInQueue(NamePlayer,Game,Timefinish,IdPostChoix);
+                    }else {
+                        player.addplayer(NamePlayer,Game,IdPostChoix,Timefinish);
+                    }
                     continue;
                     //return;
                 case 2:
                     //System.out.println("attender version");
                     if(player.getAllplayer().size()==0){
-                        System.out.println("------------------Vide list---------------");
+                        System.out.println("------------------Vide Player list---------------");
                     }else {
-                        System.out.println("-------------Player list -------------");
+                        System.out.println("-------------Player list --------------");
                         ArrayList<DetailsPlayer> listPlayer = player.getAllplayer();
+                        listPlayer.forEach((DetailsPlayer Oneplayer)->{
+                            System.out.println("Name Player: " +Oneplayer.getNamePlayer()+" || Game: "+Oneplayer.getGame()+ " || Time Finished: "+Oneplayer.timeFinal()+" || in : Post: "+String.valueOf(Oneplayer.getIdPost()+1));
+                        });
+                    }
+
+                    if(queueClass.getallQueueInList().size()==0){
+                        System.out.println("------------------Vide Queues list---------------");
+                    }else {
+                        System.out.println("-------------Queues list -------------");
+                        ArrayList<DetailsPlayer> listPlayer =queueClass.getallQueueInList();
                         listPlayer.forEach((DetailsPlayer Oneplayer)->{
                             System.out.println("Name Player: " +Oneplayer.getNamePlayer()+" || Game: "+Oneplayer.getGame()+ " || Time Finished: "+Oneplayer.timeFinal()+" || in : Post: "+String.valueOf(Oneplayer.getIdPost()+1));
                         });
@@ -96,7 +114,6 @@ public class Affichage {
             }
         }
     }
-
     public ArrayList<DetailsPosts> choosePostDispoible(GameDetails game , Posts posts){
         ArrayList<DetailsPosts> postdisponible = new ArrayList<>();
         int[] idPost = game.getIdPost();
@@ -105,7 +122,7 @@ public class Affichage {
         }
         return postdisponible;
     }
-    public int choosePost(ArrayList<DetailsPosts> postDisponible,Player player){
+    public int choosePost(ArrayList<DetailsPosts> postDisponible,Player player,Queues queues){
        while (true){
            System.out.println("this is Post disponible in your Game :\nchoose your post :");
            ArrayList<DetailsPlayer> allplayer = player.getAllplayer();
@@ -113,8 +130,12 @@ public class Affichage {
                if(allplayer.size()!=0){
                    String posteDetail = "Post"+post.getId()+": "+"Ecran=>"+post.getEcran()+" and Console=>"+post.getConsole();
                    for(DetailsPlayer players:allplayer){
-                       if(post.getId()==players.getIdPost()+1){
-                           String timefinal = player.getPlayerByHashMap(players.getIdPost()).get(players.getIdPost()).timeFinal();
+                       String timefinal;
+                       if (queues.getQueueByid(post.getId()-1)!=null) {
+                           timefinal= String.valueOf(queues.getLastQueueInPostByID(post.getId()));
+                           posteDetail+= "\033[0;33m ------playing-----\033[0m"+"end in: \033[0;32m"+timefinal+"\033[0m";
+                       }else if(post.getId()==players.getIdPost()+1){
+                            timefinal = player.getPlayerByid(players.getIdPost()).timeFinal();
                            posteDetail+= "\033[0;33m ------playing-----\033[0m"+"end in: \033[0;32m"+timefinal+"\033[0m";
                            break;
                        }
@@ -154,23 +175,28 @@ public class Affichage {
         return true;
     }
 
-    public int chooseCreneau(Horaires time,int idpost,Player playerClass){
+    public int chooseCreneau(Horaires time,int idpost,Player playerClass,Queues queues){
         while (true) {
             System.out.println("choisi votre heure :");
             ArrayList<DetailsHoraires> allcreneau = time.getallTime();
             int i = 1;
             ArrayList<Integer> idcreneauvalid = new ArrayList<>();
-            HashMap<Integer,DetailsPlayer> allplayer = playerClass.getPlayerByHashMap(idpost);
             LocalTime Timeend;
-            if(allplayer.containsKey(idpost)){
-                 Timeend = LocalTime.parse(playerClass.getPlayerByHashMap(idpost).get(idpost).timeFinal());
+            if(queues.getQueueByid(idpost)!=null){
+                Timeend= queues.getLastQueueInPostByID(idpost);
+                //Timeend = LocalTime.parse(queues.getQueueByid(idpost).timeFinal());
+            }else if(playerClass.getPlayerByid(idpost)!=null){
+                 Timeend = LocalTime.parse(playerClass.getPlayerByid(idpost).timeFinal());
             }else{
                 Timeend = LocalTime.now();
             }
+
             for (DetailsHoraires creneau : allcreneau) {
                 int finishTime = (Timeend.plusMinutes(creneau.getTimeParminute())).getHour();
                 int nowTime = LocalTime.now().getHour();
-                if ((nowTime<1 || nowTime <= 13 && nowTime > 12 || nowTime > 16) || (finishTime<1 || finishTime<=13 && finishTime>12 || finishTime>16)  ) {
+                if (nowTime>9 && nowTime<12 && (finishTime>12) || finishTime<9) {
+                    System.out.println(i + ": " + creneau.name + "\u001B[31m------invaliable-----\033[0m");
+                } else if (((nowTime>=12 && nowTime<14) || (nowTime>14 && (finishTime>=20) ) || finishTime<9) ) {
                     System.out.println(i + ": " + creneau.name + "\u001B[31m------invaliable-----\033[0m");
                 } else {
                     System.out.println(i + ": " + creneau.name+"\033[0;32m------disponibe-----\033[0m");
@@ -181,7 +207,7 @@ public class Affichage {
             String message = "\u001B[31m---your choix invalid---\033[0m";
             if(idcreneauvalid.size()==0){
                 try {
-                    System.out.println("see you tomorrow inchaelah");
+                    System.out.println("see you inchaelah");
                     Thread.sleep(2000);
                     return 0;
                 } catch (InterruptedException e) {
@@ -214,5 +240,17 @@ public class Affichage {
         }
 
     }
+
+    public boolean checkplayer(int idpost,Player playerClass){
+
+        for (DetailsPlayer player:playerClass.getAllplayer()) {
+            if(player.getIdPost()==idpost){
+                return true;
+            }else{
+               continue;
+            }
+        }
+        return false;
+   }
 
 }
