ea99e73bcc1658fe561178333c75c123bec3b165
abdessalm staili <abdoycode@gmail.com>
2022-09-27 20:47:20 +0100

    add method get last timefinish the Player

--- src/Player/Player.java
+++ src/Player/Player.java
@@ -1,23 +1,41 @@
 package Player;
 
+import java.time.LocalTime;
 import java.util.ArrayList;
-import java.util.HashMap;
 
 public  class Player{
 
     ArrayList<DetailsPlayer> allplayer = new ArrayList<>();
-    HashMap<Integer,DetailsPlayer> AllplayerinHashMap = new HashMap<>();
 
     public void addplayer(String namePlayer, String game, int idpost, String timefinish){
         allplayer.add(new DetailsPlayer(namePlayer,game,timefinish,idpost));
-        AllplayerinHashMap.put(idpost,new DetailsPlayer(namePlayer,game,timefinish,idpost));
     }
 
     public ArrayList<DetailsPlayer> getAllplayer() {
             return allplayer;
     }
 
-    public HashMap<Integer,DetailsPlayer> getPlayerByHashMap(int idpost){
-     return AllplayerinHashMap;
+    public DetailsPlayer getPlayerByid(int idpost){
+        for (DetailsPlayer player:allplayer) {
+            if(player.getIdPost()==idpost){
+                return player;
+            }
+        }
+        return null;
     }
+
+    public LocalTime getLastPlayerinPostByID(int idpost){
+        LocalTime timefich = LocalTime.now();
+        for (DetailsPlayer player:allplayer) {
+            if(player.getIdPost()==idpost){
+                System.out.println(timefich.getHour());
+                System.out.println(LocalTime.parse(player.timeFinal()).getHour());
+               if(timefich.getHour()<LocalTime.parse(player.timeFinal()).getHour()){
+                   timefich=LocalTime.parse(player.timeFinal());
+               }
+            }
+        }
+        return timefich;
+    }
+
 }
